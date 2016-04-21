c6e6e3e764c4b5fe23fb33e4862909e7f18d5ca1
ewagyae <ewagyae@windows10.microdone.cn>
2016-04-21 15:48:28 +0800

    .

--- run-ci.sh
+++ run-ci.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 python ./hello_world.py
-python ./re_filter.py 23
 python ./exc_class.py
 python ./exc_class2.py
-python ./exc_file.py
+python ./exc_file.py 23
 python ./exc_list.py
 python ./exc_time.py

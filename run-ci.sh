8f0d3bcd90b674d7243147c55b083ece582a304c
ewagyae <ewagyae@CN00081226.ericsson.se>
2016-05-04 15:28:58 +0800

    .

--- run-ci.sh
+++ run-ci.sh
@@ -6,4 +6,15 @@ python ./exc_class2.py
 python ./exc_file.py 23
 python ./exc_list.py
 python ./exc_time.py
-for f in $(ls leetcode/*py); do python $f; done
+
+##for f in $(ls leetcode/*py); do python $f; done
+
+cd leetcode
+
+for f in $(ls ./*py); do 
+   python $f 
+   status=$?
+   if [ $status != 0 ]; then
+      exit -1
+   fi
+done 
